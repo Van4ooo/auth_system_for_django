c75b8b4511669a6a1d141a66895b0d51bc246a7a
Van4o <ivan2005rubachyk@gmail.com>
2022-06-26 14:23:54 +0300

    add 4 tests for home page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+    def test_status_code(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_by_name_status_code(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse("home"))
+        self.assertTemplateUsed(resp, "home.html")
+
+    def test_contains_in_home_page(self):
+        resp = self.client.get(reverse("home"))
+        self.assertContains(resp, 'You are not logged in')
