751ffcec075e3d840756e3624e2a81ccdd00cd1a
Van4o <ivan2005rubachyk@gmail.com>
2022-06-28 16:49:23 +0300

    add roure url for edit Article

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', ArticlePageView.as_view(), name="articles"),
     path('<int:pk>/', ArticleDetailPageView.as_view(), name="article_detail"),
-    path('<int:pk>/delete', ArticleDeletePageView.as_view(), name="article_delete")
+    path('<int:pk>/delete', ArticleDeletePageView.as_view(), name="article_delete"),
+    path('<int:pk>/edit', ArticleEditPageView.as_view(), name="article_edit"),
 ]
